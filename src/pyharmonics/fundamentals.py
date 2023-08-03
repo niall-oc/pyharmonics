833bc9eba018d0eebb4ac796581f3f74d6d53151
niall-oc <zechs.marquie@gmail.com>
2023-08-03 21:17:22 +0100

    Pain prices added to fundamentals plots

--- src/pyharmonics/fundamentals.py
+++ src/pyharmonics/fundamentals.py
@@ -3,7 +3,7 @@ __author__ = 'github.com/niall-oc'
 import yfinance as yf
 
 class YahooOptions:
-    def __init__(self, option_chain, price, top):
+    def __init__(self, option_chain, top):
         """
         :param yfinance.Ticker ticker: the yfinance Ticker object representing an asset.
         :params int top: the top 30 options, ranked by openInterest, are analyzed by default.
@@ -13,21 +13,28 @@ class YahooOptions:
         self.calls = self.calls.sort_values(by=['strike'])
         self.calls['oi_cumsum'] = self.calls['openInterest'].cumsum()
         limit = min(self.calls['strike'])
-        self.calls['losses'] = self.calls.apply(lambda row: (row['strike'] - limit) * row['oi_cumsum'], axis=1)
+        self.calls['losses'] = self.calls.apply(lambda row: (row['strike'] - limit) * row['oi_cumsum'] * 100, axis=1)
 
         self.puts = option_chain.puts
         self.puts = self.puts.sort_values(by=['openInterest', 'volume'], ascending=False)[:top]
         self.puts = self.puts.sort_values(by=['strike'])
         self.puts['oi_cumsum'] = self.puts.loc[::-1, 'openInterest'].cumsum()[::-1]
         limit = max(self.puts['strike'])
-        self.puts['losses'] = self.puts.apply(lambda row: (limit - row['strike']) * row['oi_cumsum'], axis=1)
+        self.puts['losses'] = self.puts.apply(lambda row: (limit - row['strike']) * row['oi_cumsum'] * 100, axis=1)
+
+        pain_df = self.calls[['strike', 'losses']].merge(self.puts[['strike', 'losses']], on='strike', how='outer').sort_values(by='strike')
+        pain_df['losses_x'] = pain_df['losses_x'].fillna(0.0)
+        pain_df['losses_y'] = pain_df['losses_y'].fillna(0.0)
+        pain_df['pain'] = pain_df.apply(lambda x: x['losses_x'] + x['losses_y'], axis=1)
+        self.min_pain = list(pain_df.loc[pain_df['pain'] == min(pain_df['pain'])].to_dict()['strike'].values())[0]
+        self.losses = pain_df
 
 class YahooEarnings:
     def __init__(self, ticker):
         pass
 
 class YahooFundamentals:
-    def __init__(self, symbol, top=30):
+    def __init__(self, symbol):
         self.symbol = symbol
         self.ticker = yf.Ticker(self.symbol)
         self.options = {}
@@ -41,7 +48,7 @@ class YahooFundamentals:
         call_strikes = set()
         put_strikes = set()
         for expiry in self.ticker.options:
-            self.options[expiry] = YahooOptions(self.ticker.option_chain(expiry), self.price, top)
+            self.options[expiry] = YahooOptions(self.ticker.option_chain(expiry), top)
             call_strikes = call_strikes | set(self.options[expiry].calls['strike'])
             put_strikes = put_strikes | set(self.options[expiry].puts['strike'])
         self.call_strikes = list(sorted(call_strikes))
--- src/pyharmonics/plotter/option.py
+++ src/pyharmonics/plotter/option.py
@@ -2,12 +2,14 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 class OptionPlotter:
-    def __init__(self, symbol, expiry, options, price):
-        self.calls = options.calls
-        self.puts = options.puts
+    def __init__(self, fundamentals, expiry):
+        self.calls = fundamentals.options[expiry].calls
+        self.puts = fundamentals.options[expiry].puts
+        self.losses = fundamentals.options[expiry].losses
         self.expiry = expiry
-        self.price = price
-        self.symbol = symbol
+        self.price = fundamentals.price
+        self.symbol = fundamentals.symbol
+        self.min_pain = fundamentals.options[expiry].min_pain
         self.fonts = dict(
             font=dict(
                 family="Courier New, monospace, bold",
@@ -15,16 +17,34 @@ class OptionPlotter:
             ),
             title_font_size=30
         )
+        self.colors = {
+            'calls': {  # formed
+                'line': 'rgba(0, 255, 0, 0.6)',
+                'fill': 'rgba(0, 255, 0, 0.15)'
+            },
+            'puts': {  # forming
+                'line': 'rgba(255, 127, 0, 0.6)',
+                'fill': 'rgba(255, 127, 0, 0.15)'
+            },
+            'losses': {
+                'line': 'rgba(255, 255, 255, 0.6)',
+                'fill': 'rgba(255, 255, 255, 0.05)',
+            }
+        }
         self.title = f"{self.symbol} - {self.expiry}"
         self.set_main_plot()
-        self.plot_trend('openInterest', 1)
-        self.plot_trend('losses', 2)
+        self.plot_trend('openInterest', 1, 1)
+        self.plot_price(self.price, 'Current price', 'openInterest', 1, 1)
+        self.plot_trend('losses', 2, 1)
+        self.plot_price(self.min_pain, 'Minimum_pain', 'losses', 2, 1)
+        self.plot_price(self.price, 'Current price', 'losses', 2, 1, height=0.4)
+        self.plot_losses(2, 1)
 
     def set_main_plot(self):
         self.main_plot = make_subplots(
-            rows=2, shared_xaxes=True,
+            rows=2, cols=2, shared_xaxes=True,
             vertical_spacing=0.025,
-            row_heights=[.5, .5]
+            row_heights=[.5, .5], column_widths=[.9, .1]
         )
         self.main_plot.update_layout(
             xaxis_rangeslider_visible=False,
@@ -40,48 +60,56 @@ class OptionPlotter:
             **self.fonts
         )
 
-    def plot_current_price(self):
+    def plot_losses(self, row, col):
         self.main_plot.add_trace(
             go.Scatter(
-                mode='lines+text',
-                x=[self.price * .999, self.price],
-                y=[max(self.calls['openInterest']) * 0.8, max(self.calls['openInterest']) * 0.8],
-                line=dict(color='white', width=2),
-                fill='tozeroy',
-                text=['', f"Current Price: {self.price}"]
+                mode='lines+markers',
+                x=self.losses['strike'],
+                y=self.losses['pain'],
+                fillcolor=self.colors['losses']['fill'],
+                line=dict(color=self.colors['losses']['line'], width=2),
+                fill='tozeroy'
             ),
-            row=1, col=1
+            row=row, col=col
         )
 
-    def plot_trend(self, trend, row):
+    def plot_price(self, price, label, against, row, col, height=0.8):
+        h = max(self.calls[against]) * height
         self.main_plot.add_trace(
             go.Scatter(
-                x=self.calls['strike'],
-                y=self.calls[trend],
-                line=dict(color='lightgreen', width=2),
-                fill='tozeroy'
-            ),
-            row=row, col=1
-        )
-        self.main_plot.add_trace(
-            go.Scatter(
-                x=self.puts['strike'],
-                y=self.puts[trend],
-                line=dict(color='red', width=2),
-                fill='tozeroy'
-            ),
-            row=row, col=1
-        )
-        self.main_plot.add_trace(
-            go.Scatter(
-                mode='lines+text',
-                x=[self.price * .999, self.price],
-                y=[max(self.calls[trend]) * 0.8, max(self.calls[trend]) * 0.8],
+                mode='lines+markers+text',
+                x=[price * .9999, price],
+                y=[h, h],
                 line=dict(color='white', width=2),
                 fill='tozeroy',
-                text=['', f"Current Price: {self.price}"]
+                text=['', f"{label}: {price}"],
+                textposition="top center"
             ),
-            row=row, col=1
+            row=row, col=col
+        )
+
+    def plot_trend(self, trend, row, col):
+        self.main_plot.add_trace(
+            go.Scatter(
+                mode='lines+markers',
+                x=self.calls['strike'],
+                y=self.calls[trend],
+                fillcolor=self.colors['calls']['fill'],
+                line=dict(color=self.colors['calls']['line'], width=2),
+                fill='tozeroy'
+            ),
+            row=row, col=col
+        )
+        self.main_plot.add_trace(
+            go.Scatter(
+                mode='lines+markers',
+                x=self.puts['strike'],
+                y=self.puts[trend],
+                fillcolor=self.colors['puts']['fill'],
+                line=dict(color=self.colors['puts']['line'], width=2),
+                fill='tozeroy'
+            ),
+            row=row, col=col
         )
 
     def show(self):
